169121b61e39f5c3c063bd1a7ba2c1065b02676f
yun010207 <94025225+yun010207@users.noreply.github.com>
2025-05-15 17:48:36 +0900

    Update modeling_yolov.py

--- modeling_yolov.py
+++ modeling_yolov.py
@@ -1,20 +1,21 @@
 from ultralytics import YOLO
 
 def main():
-    model = YOLO("yolov8s-seg.pt") #자신이 사용할 모델 넣기
+
+
+    model = YOLO("yolov8n-seg.pt") #자신이 사용할 모델 넣기
     model.train(
-        data=r"C:\Users\User\Desktop\total\dataset\data.yaml",
-        epochs=100,
-        imgsz=640,
-        batch=16,
-        patience=50,  # early stopping: 50 epoch 동안 개선 없으면 중단
+        data=r"C:/Users/User/Desktop/cross/dataset/data.yaml",
+        epochs=10,
+        imgsz=256,
+        batch=64,
+        patience=10,  # early stopping: 10 epoch 동안 개선 없으면 중단
         save_period=5,  # 5 epochs마다 모델 저장
-        name="yolov8s-seg_custom_again"
+        workers=8,
+        name="yolov8n-seg_crosswalk"
     )
 
 if __name__ == '__main__':
     from multiprocessing import freeze_support
     freeze_support()
     main()
-
-
