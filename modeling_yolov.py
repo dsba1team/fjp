919db054fc8a41df462a5dd1368fcfa03585b76e
yun010207 <94025225+yun010207@users.noreply.github.com>
2025-05-26 09:10:45 +0900

    Add files via upload

--- modeling_yolov.py
+++ modeling_yolov.py
@@ -2,17 +2,19 @@ from ultralytics import YOLO
 
 def main():
 
-
-    model = YOLO("yolov8n-seg.pt") #자신이 사용할 모델 넣기
+    model = YOLO(r"C:\Users\User\PycharmProjects\PythonProject1\runs\segment\yolo11n-seg_finfin\weights\last.pt") #자신이 사용할 모델 넣기
     model.train(
-        data=r"C:/Users/User/Desktop/cross/dataset/data.yaml",
-        epochs=10,
-        imgsz=256,
-        batch=64,
-        patience=10,  # early stopping: 10 epoch 동안 개선 없으면 중단
-        save_period=5,  # 5 epochs마다 모델 저장
-        workers=8,
-        name="yolov8n-seg_crosswalk"
+        task="segment",  # 또는 'detect'
+        data=r"C:\Users\User\Desktop\dataset_finfin\data.yaml",
+        epochs=300,
+        imgsz=640,
+        batch=16,
+        patience=20,
+        save_period=20,
+        device=0,  # GPU 번호
+        workers=8,  # 워커 수: multiprocessing 에러가 계속 나면 0으로 낮춰보세요
+        resume=True,
+        name="yolo11n-seg_finfin"
     )
 
 if __name__ == '__main__':
